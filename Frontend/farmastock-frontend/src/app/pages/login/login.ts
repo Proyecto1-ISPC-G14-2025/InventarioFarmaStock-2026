23a39f532401f311e49ae9e4498dc65be3b41e53
jorgelinasapp <jorgelinasapp@gmail.com>
2026-06-03 18:29:49 -0300

    Avances login

--- Frontend/farmastock-frontend/src/app/pages/login/login.ts
+++ Frontend/farmastock-frontend/src/app/pages/login/login.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
 import {
   ReactiveFormsModule,
   FormBuilder,
   FormGroup,
   Validators
 } from '@angular/forms';
-
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -26,23 +25,10 @@ export class Login {
 
   loginForm!: FormGroup;
 
-  usuarios: Record<string, { password: string; role: string }> = {
-
-    'admin@correo.com': {
-      password: 'admin123',
-      role: 'admin'
-    },
-
-    'usuario@correo.com': {
-      password: 'user123',
-      role: 'user'
-    },
-
-  };
-
   constructor(
     private router: Router,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private http: HttpClient
   ) {
 
     this.loginForm = this.fb.group({
@@ -82,33 +68,48 @@ export class Login {
 
     const { email, password } = this.loginForm.value;
 
-    const user = this.usuarios[email ?? ''];
+    this.http.post<any>(
+      'http://localhost:8000/api/auth/login/',
+      {
+        username: email,
+        password: password
+      }
+    ).subscribe({
 
-    if (user && user.password === password) {
+      next: (data) => {
 
-      if (user.role === 'admin') {
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('rol', data.rol);
+        localStorage.setItem('nombre', data.nombre);
+        localStorage.setItem('user_id', data.user_id);
 
-        this.router.navigate(['/admin']);
+        if (data.rol === 'admin') {
 
-      } else {
+          this.router.navigate(['/admin']);
 
-        this.router.navigate(['/usuario']);
+        } else {
+
+          this.router.navigate(['/usuario']);
+
+        }
+
+      },
+
+      error: () => {
+
+        this.loginError = true;
+
+        this.shaking = true;
+
+        setTimeout(() => {
+
+          this.shaking = false;
+
+        }, 600);
 
       }
 
-    } else {
-
-      this.loginError = true;
-
-      this.shaking = true;
-
-      setTimeout(() => {
-
-        this.shaking = false;
-
-      }, 600);
-
-    }
+    });
 
   }
 
