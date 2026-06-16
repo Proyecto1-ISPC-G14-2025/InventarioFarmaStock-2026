e3f0bb28c2bd886a57067613e130cbe78b7c44fe
Kevin <kevin.agustin.astrada@gmail.com>
2026-06-16 17:53:34 -0300

    fix: corregir admin

--- Backend/inventario/admin.py
+++ Backend/inventario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Medicamento, Proveedor, Perfil, Movimiento
 
-# Register your models here.
+admin.site.register(Medicamento)
+admin.site.register(Proveedor)
+admin.site.register(Perfil)
+admin.site.register(Movimiento)
